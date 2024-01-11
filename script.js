f46d975e0b12cc8dc33f0629f682e10b1df44e6b
Mikael Torndahl <mikaeltorndahl@gmail.com>
2024-01-12 00:47:38 +0100

    Progress, the game-player can move up and down and the unit keeps on going continuesly

--- script.js
+++ script.js
@@ -2,24 +2,39 @@
 
 let spelare;
 const hinder = [];
+let canvasBredd;
+let canvasHojd;
 
 class SpelarEnhet {
   constructor(bredd, hojd, farg, posX, posY) {
     this.bredd = bredd;
     this.hojd = hojd;
-    this.hastighetX = 0;
+    this.farg = farg;
+    this.hastighetX = 1;
     this.hastighetY = 0;
     this.posX = posX;
     this.posY = posY;
-    this.uppdatera = function (enhet) {
-      spelMiljo.context;
-      enhet.fillStyle = farg;
-      enhet.fillRect(this.posX, this.posY, this.bredd, this.hojd);
-    };
-    this.nyPos = function () {
-      this.posX += this.hastighetX;
-      this.posY += this.hastighetY;
-    };
+  }
+  uppdatera(enhet) {
+    enhet.fillStyle = this.farg;
+    enhet.fillRect(this.posX, this.posY, this.bredd, this.hojd);
+  }
+
+  nyPos() {
+    this.nyPosX = this.posX + this.hastighetX;
+    this.nyPosY = this.posY + this.hastighetY;
+
+    if (this.nyPosY < 0) {
+      this.posY = 0;
+    } else if (this.nyPosY > canvasHojd - this.hojd) {
+      this.posY = canvasHojd - this.hojd;
+    } else {
+      this.posY = this.nyPosY;
+    }
+
+    if (this.nyPosX >= 0 && this.nyPosX <= canvasBredd - this.bredd) {
+      this.posX = this.nyPosX;
+    }
   }
 }
 
@@ -27,39 +42,57 @@ const spelRuta = document.getElementById("spelRuta");
 
 const spelMiljo = {
   canvas: document.createElement("canvas"),
+
   starta: function () {
-    this.canvasBredd = 1200;
-    this.canvasHojd = 800;
     this.context = this.canvas.getContext("2d");
     spelRuta.append(this.canvas);
+
+    window.addEventListener("keydown", pilStyrning);
+
     this.frameNo = 0;
     this.intervall = setInterval(uppdateraSpelMiljo, 10);
   },
   rensa: function () {
-    this.context.clearRect(0, 0, this.canvasBredd, this.canvasHojd);
+    this.context.clearRect(
+      spelare.posX,
+      spelare.posY,
+      spelare.bredd,
+      spelare.hojd
+    );
   },
   stop: function () {
     clearInterval(this.intervall);
   },
 };
 
-const uppdateraSpelMiljo = () => {
+function uppdateraSpelMiljo() {
   spelMiljo.rensa();
   spelare.nyPos();
   spelare.uppdatera(spelMiljo.context);
-};
+}
+
+function pilStyrning(event) {
+  if (event.key === "ArrowUp") {
+    spelare.hastighetY = -2;
+  } else if (event.key === "ArrowDown") {
+    spelare.hastighetY = 2;
+  }
+}
 
 const startKnapp = document.getElementById("startKnapp");
-const projektHeader = document.getElementById("projektHeader");
 const info = document.getElementById("info");
+
 startKnapp.addEventListener("click", () => {
   startaSpel();
   startKnapp.style.display = "none";
-  projektHeader.style.display = "none";
   info.style.display = "none";
 });
 
 function startaSpel() {
-  spelare = new SpelarEnhet(20, 20, "blue", 10, 70);
+  canvasBredd = window.innerWidth;
+  canvasHojd = spelRuta.clientHeight;
+  spelMiljo.canvas.width = canvasBredd;
+  spelMiljo.canvas.height = canvasHojd;
+  spelare = new SpelarEnhet(50, 40, "#019ba3f7", 60, 60);
   spelMiljo.starta();
 }
